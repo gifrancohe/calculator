e47ad65be21276ba46491db660ef3ed23128b188
Giovanny Franco <giovannyfranco@mattelsa.net>
2020-03-13 19:49:44 -0500

    Create and execute functions to all events of the calculator

--- js/app.js
+++ js/app.js
@@ -66,4 +66,108 @@ var Calculator = {
     changeSign: () => {
         Calculator.displayValue = (Calculator.displayValue * -1);
     },
+    /**
+     * Function to update the screen
+     */
+    updateScreen: () => {
+        var screen = document.getElementById('display'); //screen value
+        screen.textContent = Calculator.displayValue;
+    },
+    /**
+     * Function to determine the operation to perform
+     */
+    operations: (key, value1, value2) => {
+        switch (key) {
+            case "mas":
+                Calculator.result = Calculator.addition( value1, value2);
+                break;
+            case "menos":
+                Calculator.result = Calculator.subtraction( value1, value2);
+                break;
+            case "por":
+                Calculator.result = Calculator.multiplication( value1, value2);
+                break;
+            default:
+                Calculator.result = Calculator.division( value1, value2);
+                break;
+        }
+    },
+    /**
+     * Function to determine the result of the operation
+     */
+    equal: () => {
+        if(Calculator.equalFlag) {
+            Calculator.value2 = Calculator.displayValue;
+            Calculator.lastValue = Calculator.displayValue;
+            Calculator.operations(Calculator.operator, Calculator.value1, Calculator.value2);
+        }else {
+            Calculator.operations(Calculator.operator, Calculator.value1, Calculator.lastValue);
+        }
+
+        Calculator.value1 = Calculator.result;
+        Calculator.equalFlag = false;
+        Calculator.displayValue = Calculator.result;
+        Calculator.updateScreen();
+    },
+    /**
+     * Function to determine the event occurred in the calculator
+     */
+    functions: (key) => {
+        switch (key) {
+            case "On":
+                Calculator.onCFunction();
+                break;
+            case "signo":
+                Calculator.changeSign();
+                break;
+            case "punto":
+                Calculator.validatePoint(Calculator.displayValue);
+                break;
+            case "igual":
+                Calculator.equal();
+                break;
+            default:
+                Calculator.value1 = Calculator.displayValue;
+                Calculator.operator = key;
+                Calculator.displayValue = "";
+                Calculator.equalFlag = true;
+                break;
+        }
+        Calculator.updateScreen();
+    },
+    /**
+     * Function that executes  any button  on the calculator is pressed
+     * and validates if the button is a number or a special key
+     */
+    execute: (element) => {
+        let key = element.alt;
+        if(/^([0-9])*$/.test(key)) { //Validate if the key pressed is a number
+            if(Calculator.validateZero(key, Calculator.displayValue)) {
+                return;
+            }else {
+                if(Calculator.displayValue.toString().length <= 8) {
+                    if(Calculator.displayValue != "0")
+                        Calculator.displayValue = Calculator.displayValue + key;
+                    else
+                        Calculator.displayValue = key;
+                }
+            }
+            Calculator.updateScreen();
+        }else {
+            Calculator.functions(key);
+        }
+    },
+    /**
+     * Funtion that initializes and assigns the execute function to all the calculator buttons
+     */
+    init: (function() {
+		var teclas = document.getElementsByClassName('tecla');
+        for (var i = 0; i < teclas.length; i++) {
+            teclas[i].addEventListener('click', function() {
+                Calculator.execute(this);
+            });
+        }
+	})
 }
+//The init method is executed
+Calculator.init();
